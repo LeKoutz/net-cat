d36b7ba5506c981a49cabef22ec347def6a363c0
cpatelis <patelischristos@gmail.com>
2026-03-16 18:37:07 +0200

    func getName

--- client/client.go
+++ client/client.go
@@ -1,17 +1,19 @@
 package client
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 )
 
+// Client struct stores connection and Username
 type Client struct {
 	Conn net.Conn // η "γραμμή" σύνδεσης TCP
 	Name string   // το όνομά του
 }
 
 const Logo = `
-	     _nnnn_
+         _nnnn_
         dGGGGMMb
        @p~qp~~qMb
        M|@||@) M|
@@ -30,11 +32,26 @@ _)      \.___.,|     .'
 
  `
 
-func sendWelcome(conection net.Conn) {
-	fmt.Fprint(conection, "Welcome to TCP-Chat!\n", Logo, "\n[ENTER YOUR NAME]:")
+// Welcome message with logo
+func sendWelcome(conn net.Conn) {
+	fmt.Fprint(conn, "Welcome to TCP-Chat!\n", Logo, "\n[ENTER YOUR NAME]:")
 
 }
 
-func HandleConnection(conection net.Conn) {
-	sendWelcome(conection)
+func HandleConnection(conn net.Conn) {
+	sendWelcome(conn)
+}
+
+// Get users name
+func GetName(conn net.Conn) string {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		name := scanner.Text()
+		if name == "" {
+			fmt.Fprint(conn, "[ENTER YOUR NAME]:")
+			continue
+		}
+		return name
+	}
+	return ""
 }
